59c089ed05572eb00455d4476b12ec3d22d376de
IlhamShiddiq <shdqillham123@gmail.com>
2021-05-10 19:57:58 +0700

    Table structures

--- admin_user/models.py
+++ admin_user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class publishers(models.Model):
+    publisher = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.publisher
+
+class categories(models.Model):
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
+class books(models.Model):
+    isbn = models.CharField(max_length=20)
+    title = models.CharField(max_length=100)
+    publish_year = models.IntegerField()
+    stock = models.IntegerField()
+    writer = models.CharField(max_length=100)
+    publisher = models.ForeignKey(publishers, on_delete=models.CASCADE)
+    category = models.ForeignKey(categories, on_delete=models.CASCADE)
+    synopsis = models.TextField()
+    image = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.title
+
+class members(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    address = models.TextField()
+    image = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.name
--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from admin_user.models import members, books
 
-# Create your models here.
+class transactions(models.Model):
+    member = models.ForeignKey(members, on_delete=models.CASCADE)
+    borrow_date = models.DateField()
+
+class detail_transactions(models.Model):
+    transaction = models.ForeignKey(transactions, on_delete=models.CASCADE)
+    book = models.ForeignKey(books, on_delete=models.CASCADE)
+    is_returned = models.CharField(max_length=1)
+    return_of_date = models.DateField(null=True)
+    is_ontime = models.CharField(max_length=1, null=True)
